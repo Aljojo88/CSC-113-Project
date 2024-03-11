9f7bd316d1d1e7fa6bde5bfdc2d505f4390a6409
Asma-BinJameel <162357634+Asma-BinJameel@users.noreply.github.com>
2024-03-11 14:22:29 +0300

    Update Nails.java

--- Project/src/Nails.java
+++ Project/src/Nails.java
@@ -1,4 +1,33 @@
 
-public class Nails {
+public class Nails extends Appointment{
+   private char type;
 
+   public Nails(String day, int time, char type){
+      super(day, time);
+      this.type = type;
+   }
+
+   public double CalculatePrice(){
+      if ((type=='c')||(type=='S'))
+         return time*80.0;
+      else if ((type=='a')||(type=='A'))
+         return time*90.0;
+      else if ((type=='c')||(type=='C'))
+         return time*100.0;
+      else
+         return 0;
+   }
+   
+ public char getType(){
+ 
+ return type;
+ 
+ 
+ }  
+   
+   
+   
+   
+   
+   
 }
