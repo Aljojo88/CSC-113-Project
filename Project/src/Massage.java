702865195af3fb4765bb9ac74d25e01e888ca3f2
Asma-BinJameel <162357634+Asma-BinJameel@users.noreply.github.com>
2024-03-11 14:19:19 +0300

    Update Massage.java

--- Project/src/Massage.java
+++ Project/src/Massage.java
@@ -1,4 +1,33 @@
 
-public class Massage {
+public class Massage extends Appointment{
+   
+   
+   private char type;
 
+   public Massage(String day, int time, char type){
+      super(day, time);
+      this.type = type;
+   }
+
+   public double CalculatePrice(){
+      if ((type=='s')||(type=='S'))
+         return time*80.0;
+      else if ((type=='a')||(type=='A'))
+         return time*90.0;
+      else if ((type=='c')||(type=='C'))
+         return time*100.0;
+      else 
+         return 0;
+   }
+   
+   
+   
+ public char getType(){
+ 
+ return type;
+}   
+   
+   
+   
+   
 }
