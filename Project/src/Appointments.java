e09a2882e1bdfc043c38db8ae069a955a2563ef9
Asma-BinJameel <162357634+Asma-BinJameel@users.noreply.github.com>
2024-03-11 14:21:00 +0300

    Update Appointments.java

--- Project/src/Appointments.java
+++ Project/src/Appointments.java
@@ -1,4 +1,39 @@
 
 public class Appointments {
+protected String date;
+   protected int time;
+
+   public Appointment(String date, int time){
+      this.date = date;
+      this.time = time;
+   }
+   
+   /*public Appointment(Appointment b){
+   this.day = b.day;
+   this.time = b.time;
+   }*/
+
+   public abstract double CalculatePrice();
+
+   public String toString(){
+      return date + " at " + time;
+   }
+
+public String getDate(){
+
+return date;
+
+}
+
+public int getTime(){
+
+return time;
+
+}
+
+
+
+
+
 
 }
