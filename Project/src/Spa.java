2954953c698f9c0310b5a2eed8300c506e40f90a
Asma-BinJameel <162357634+Asma-BinJameel@users.noreply.github.com>
2024-03-11 14:21:59 +0300

    Update Spa.java

--- Project/src/Spa.java
+++ Project/src/Spa.java
@@ -4,42 +4,41 @@ public class Spa {
 	private String name;
 	private String branch;
 	private int noOfC;
-	private Customer customer [];
+	private Customer customers [];
 	
-	public Spa(name , branch , size) {
+	public Spa(String name , String branch , int size) {
 		this.name=name;
 		this.branch=branch;
-		customer = new Customer [size];
+		customers = new Customer[size];
 	}
 	
-	public Spa( Spa S ) {
+	/*public Spa( Spa S ) {
 		this.name=S.name;
 		this.branch=S.branch;
 		this.noOfC=S.noOfC;
-
-		
+      
 		Customer arr [] = new Customer[S.customer.length];
 		
 		for(int i=0 ; i<noOfC ; i++)
 			arr[i]=customer[i]; //revise
 					
-	}
+	}*/
       
 	public int CalculatePoint() {
 		int point = 0;
 		
-	  for(int i=0 ; i<nOfC ; i++)
-		  if(customer[i].getTotalPrice()>1000)
+	  for(int i=0 ; i < noOfC ; i++)
+		  if(customers[i].getTotalPrice() > 1000)
 			  point = 30;
 		  else
-			  if(customer[i].getTotalPrice()<1000 && customer[i].getTotalPrice()>800)
+			  if(customers[i].getTotalPrice()<1000 && customers[i].getTotalPrice()>800)
 				  point = 20;
 			  else
-				  if(customer[i].getTotalPrice()<800 && customer[i].getTotalPrice()>500)
+				  if(customers[i].getTotalPrice()<800 && customers[i].getTotalPrice()>500)
 					  point = 10;
 	  
 				  else
-					  if(customer[i].getTotalPrice()<500 && customer[i].getTotalPrice()>200)
+					  if(customers[i].getTotalPrice()<500 && customers[i].getTotalPrice()>200)
 						  point = 5;
 					  else
 						  point = 0;
@@ -48,26 +47,26 @@ public class Spa {
 	}
 	
 	public boolean addCustomer(Customer C) {
-		if(noOfC < customer.length) {
-			customer[noOfC++]=C;
+		if(noOfC < customers.length) {
+			customers[noOfC++]=C;
 		return true;
 		}
 		else
 			return false;
 	}
 	
-	public boolean removeCustomer(int C) {
+	/*public boolean removeCustomer(int C) {
 		for(int i=0 ; i<noOfC ; i++)
-			if(customer[i].getIndex()==C) {
-				customer[C] = customer [customer.length-1];
+			if(customers[i].getIndex()== 'C') {
+				customers[C] = customers [customers.length-1];
 		
-		customer[--noOfC]=null;
+		customers[--noOfC]=null;
 		return true;
 			}
 		else
 			return false;
 		
-	}
+	}*/
 	
 	public void display() {
 		System.out.println("The Spa name is: "+name+", and it is located in: "+branch);
@@ -76,4 +75,9 @@ public class Spa {
 		
 	}
 	
+  
+   
+   
+}
+
 }
